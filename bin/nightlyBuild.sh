2998175ece0ac06dabfb6dde5d49434a7f8416c9
brockman <brockman@a2877cdd-62a4-4431-bd8b-17bb77e85981>
2008-12-26 02:10:31 +0000

    Updated nightly build for svn

--- bin/nightlyBuild.sh
+++ bin/nightlyBuild.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-export CVS_RSH=ssh
+#updated for svn
 
 chmod +x bin/uploadNightlyToIwphys.sh
 chmod +x bin/writeBuildVersionJava.sh
 
 echo "IWP: Performing nightly build."
 
-cvs -q update -dP && ./bin/writeBuildVersionJava.sh && ant clean && ant dist && ./bin/uploadNightlyToIwphys.sh
+svn update && ./bin/writeBuildVersionJava.sh && ant clean && ant dist && ./bin/uploadNightlyToIwphys.sh
+
+# cvs -q update -dP && ./bin/writeBuildVersionJava.sh && ant clean && ant dist && ./bin/uploadNightlyToIwphys.sh
 
 cat ./src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
 
