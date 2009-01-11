49689a6335ba575250d04c55e5b8e1e326ded406
brockman <brockman@a2877cdd-62a4-4431-bd8b-17bb77e85981>
2009-01-11 18:59:33 +0000

    Release version 4.0.3, revved trunk build version to 4.0.4

--- src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
+++ src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
@@ -1,2 +1,2 @@
 package edu.ncssm.iwp.util.buildversion;
-public class BuildVersion { public static final String VERSION = "4.0.2 Release Candidate 2009-Jan-03 16:50:43 brockman@visionisticindustries.com"; }
+public class BuildVersion { public static final String VERSION = "4.0.4 Release Candidate 2009-Jan-11 13:59:15 brockman@visionisticindustries.com"; }
