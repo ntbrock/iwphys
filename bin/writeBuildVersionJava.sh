f61aca2c1d478a55a51efa47e8cd635a1c28d2e9
brockman <brockman@a2877cdd-62a4-4431-bd8b-17bb77e85981>
2009-01-28 05:41:26 +0000

    revved ersion to 4.0.4 final public release!

--- bin/writeBuildVersionJava.sh
+++ bin/writeBuildVersionJava.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAJOR_VERSION="4.0.4 Release Candidate"
+MAJOR_VERSION="4.0.4 Public Release"
 OUT_FILE="./src/edu/ncssm/iwp/util/buildversion/BuildVersion.java"
 
 echo "package edu.ncssm.iwp.util.buildversion;" >$OUT_FILE
--- src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
+++ src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
@@ -1,2 +1,2 @@
 package edu.ncssm.iwp.util.buildversion;
-public class BuildVersion { public static final String VERSION = "4.0.4 Release Candidate 2009-Jan-11 13:59:15 brockman@visionisticindustries.com"; }
+public class BuildVersion { public static final String VERSION = "4.0.4 Public Release 2009-Jan-28 00:33:24 brockman@visionisticindustries.com"; }
