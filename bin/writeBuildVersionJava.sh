39fbac2b863136ebab97f0c00a5993405889361d
brockman <brockman@a2877cdd-62a4-4431-bd8b-17bb77e85981>
2014-01-03 04:58:46 +0000

    Instructions and build updates to make DigiCert code signing easier!

--- bin/writeBuildVersionJava.sh
+++ bin/writeBuildVersionJava.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAJOR_VERSION="4.0.5 Public Release"
+MAJOR_VERSION="4.0.5 Signed 2014-Jan-02"
 OUT_FILE="./src/edu/ncssm/iwp/util/buildversion/BuildVersion.java"
 
 echo "package edu.ncssm.iwp.util.buildversion;" >$OUT_FILE
--- src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
+++ src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
@@ -1,2 +1,2 @@
 package edu.ncssm.iwp.util.buildversion;
-public class BuildVersion { public static final String VERSION = "4.0.5 Public Release 2013-Nov-15 21:13:22 brockman@air-brockman-2013.local"; }
+public class BuildVersion { public static final String VERSION = "4.0.5 Signed 2014-Jan-02 2014-Jan-02 23:14:42 brockman@air-brockman-2013.local"; }
