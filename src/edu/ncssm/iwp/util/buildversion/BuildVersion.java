65d876125dd1f874db3a024b169d12b322e776e6
Taylor Brockman <taylor.brockman@gmail.com>
2016-01-11 18:56:34 -0500

    Retroverted to Version 4.1.2 for Signaure build

--- src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
+++ src/edu/ncssm/iwp/util/buildversion/BuildVersion.java
@@ -1,2 +1,2 @@
 package edu.ncssm.iwp.util.buildversion;
-public class BuildVersion { public static final String VERSION = "4.1.1 2015-Mar-16 11:55:23 brockman@air-brockman-2013.local"; }
+public class BuildVersion { public static final String VERSION = "4.1.2 2016-Jan-11 18:47:23 brockman@air-brockman-2013.local"; }
