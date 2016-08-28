0bad8664ae7d87a1f84f0e013d6d9d2e02c1e796
brockman <brockman@dis.local>
2016-08-28 12:59:42 -0400

    Animation cleanup

--- www-version5/publicWww/animate.php
+++ www-version5/publicWww/animate.php
@@ -4,42 +4,12 @@
 // 'New-age' php script tha trenders the right java code based on the PATH_INFO
 
 include_once('iwp-config.php');
-include_once('common.inc');
 
 
-// in url bar. Need to be respectful of the file paths.
-
-$iwpPath = $_SERVER['PATH_INFO'];
-
-// The fully formed URL to the problem file.
-// Special Cloudflare logic to detect forwarded SSL  2015Feb25 Brockman
-$httpSecure = false;
-if ( ! empty($_SERVER['HTTPS']) ) { $httpSecure = true; }
-if ( ! empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) { $httpSecure = true; }
-
-$iwpUrl = ( $httpSecure ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $prefixUri . '/' . $problemPath . $iwpPath;
-
-$iwpUrl = preg_replace("/[ ]/", "%20", $iwpUrl, -1 );
 
 ?>
+<p>animateUrl: <?= $animateUrl ?></p>
+<p>xtojUrl: <?= $xtojUrl ?></p>
+<p>iwpFile: <?= $iwpFile ?></p>
 
-<? common_header('Animate Problem'); ?>
-<br>
-
-<center>Filename: <b><?= $iwpPath ?></b></center><br>
-
-<center><applet code="edu.ncssm.iwp.bin.IWP_Applet" archive="<?= $jarUri ?>" width="400" height="200">
-<param name="problem" value="<?= $iwpUrl ?>">
-<param name="student" value="true">
-</applet></center>
-
-<br><br>
-
-
-  <center><p><i><font size="+2">Java Applet not running?</font></i></p><p>Look for a 'Plug In Blocked' <img src="/pps/gfx/plugin-blocked.png"> in the URL location bar of your web browser. You will need to give your permission to run the applet!  Upgrading to Java Version 8 is also a great idea.</p></center>
-
-<br>
-
-
-
-<? common_footer(); ?>
+<p>Description: <?= readIwpFileDescription($iwpFile) ?>
--- www-version5/publicWww/iwp-config.php
+++ www-version5/publicWww/iwp-config.php
@@ -1,20 +1,67 @@
 <?php
 // 2016-Aug-28 Brockman - IWP5 
 //
-
-// Update this with the location of the web script
-$prefixUri = '.';
-
-// fwd slash for unix. back slash for dos.
-$filesep = '/';
+//----------------------------------------------------------------------------------------
+// File Path Setup
 
 // where the problems on disk are located.
-$problemPath = "./animations/iwp-packaged"; // etc" . $filesep . "problems";
+$animationPath = "animations";
 
-// Helpful urls.
-$indexUri = $prefixUri . "/";
-$animateUri = $prefixUri . "/animate.php";
-$xtojUri = $prefixUri . "/xtoj.php";
-$problemListUri = $prefixUri . "/problemList.php";
+
+//----------------------------------------------------------------------------------------
+// Relative URL Routes
+
+$prefixUri       = '/';
+$indexUri        = $prefixUri;
+$animateUri      = $prefixUri . "animate.php";
+$xtojUri         = $prefixUri . "xtoj.php";
+$problemListUri  = $prefixUri . "problemList.php";
+
+
+//----------------------------------------------------------------------------------------
+// Web Script path setup
+
+$pathInfo = $_SERVER['PATH_INFO'];
+
+// The fully formed URL to the problem file.
+// Special Cloudflare logic to detect forwarded SSL  2015Feb25 Brockman
+$httpSecure = false;
+if ( ! empty($_SERVER['HTTPS']) ) { $httpSecure = true; }
+if ( ! empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) { $httpSecure = true; }
+
+
+$animateUrl = ( $httpSecure ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $animateUri . $pathInfo;
+$xtojUrl = ( $httpSecure ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $animateUri . $pathInfo;
+
+
+//----------------------------------------------------------------------------------------
+// Filesystem Scanning
+
+// Look for the fiel on disk
+$iwpFile =  $animationPath . $pathInfo;
+if ( ! file_exists ( $iwpFile ) ) { die("No Such File: " . $iwpFile); }
+
+$description = 'todo';
+
+function readIwpFileDescription($fullPath) { 
+	 $xml_string = file_get_contents($fullPath);
+	 $xml = simplexml_load_string($xml_string);
+	 $json = json_encode($xml);
+	 $jobject = json_decode($json,true);
+	 return $jobject['objects']['description']['text'];
+}
+
+//----------------------------------------------------------------------------------------
+// Functions
+
+// http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+function endsWith($haystack, $needle) {
+  // search forward starting from end minus needle length characters
+  return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+}
+function startsWith($haystack, $needle) {
+  // search backwards starting from haystack length characters from the end
+  return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
 
 ?>
