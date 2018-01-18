eabe28320f4856b7f94fb7d8a59a93e9a9742fdd
Matt Mims <matthewrmims@gmail.com>
2018-01-18 18:03:46 -0500

    Added basic path to graph window

--- www-version5/publicWww/js/iwp5-graph.js
+++ www-version5/publicWww/js/iwp5-graph.js
@@ -1,10 +1,10 @@
-/** 
+/**
  * Interactive Web Physics 5 - Javascript SVG Animation Implementation
  * Ryan Steed, Taylor Brockman 2016 - Version 5.0 Initial Port to HTML + SVG
  * Matthew Mims, Taylor Brockman 2018  - Version 5.1 Added Graphing and D3
  */
 
-$(function() { 
+$(function() {
 	graphInit();
 })
 
@@ -20,35 +20,43 @@ var graphYScale = d3.scaleLinear()
 
 var path = d3.path();
 
+var visualPath = null;
 
-function graphInit() { 
+function graphInit() {
 
   var svg = d3.select('#graph');
   console.log("iwp5-graph.js:15 found svg: " , svg)
-
+	visualPath = svg.append('path')
+								   .classed("iwp-graph-line", true)
+									 .attr("d", path )
+	path.moveTo(0, 0)
 }
 
-/** 
+/**
  * Performs no calculations, but repaints every thing (time, outputs, solids) onto screen from memory at current step.
  */
-function graphStep(step, vars) { 
-  
+function graphStep(step, vars) {
 
+	var lastStep = varsAtStep[step-1]
   //var vars = varsAtStep[step];
-  if ( vars == undefined ) { 
+  if ( vars == undefined ) {
     throw "No previous calculations available at step: " + step;
-  } else { 
+  } else {
 
 
 	var svg = d3.select('#graph');
-	console.log("iwp5-graph:17> regraph step with vars: ", vars)    
-
-   svg.append('circle')
+	console.log("iwp5-graph:48> currentT, currentY: ", vars.t, vars.y)
+	console.log("iwp5-graph:49> lastT, lastY: ", lastStep.t, lastStep.y)
+	path.moveTo(graphXScale(lastStep.t), graphYScale(lastStep.y))
+	path.lineTo(graphXScale(vars.t), graphYScale(vars.y))
+	//path.moveTo(graphXScale(vars.t), graphYScale(vars.y))
+	visualPath.attr("d", path)
+   /*svg.append('circle')
 //      .classed("iwp-axis-line", true)
       .attr("cx", graphXScale(vars.t))
       .attr("cy", graphYScale(vars.y))
       .attr("r", graphXScale(0.1))
-     
+	*/
 
 /*
    $.each( outputs, function( index, output ) {
@@ -62,6 +70,3 @@ function graphStep(step, vars) {
 
   }
 }
-
-
-
