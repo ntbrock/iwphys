5b478958bab690c71d541d91ea193c2d300667e2
Taylor Brockman <taylor.brockman@gmail.com>
2018-03-07 23:02:25 -0500

    Problem time iteration prototyped from controller

--- play-version6/app/controllers/DProblemController.scala
+++ play-version6/app/controllers/DProblemController.scala
@@ -6,11 +6,13 @@ import javax.inject._
 
 import edu.ncssm.iwp.math.designers.MCalculator_Abstract_subDesigner
 import edu.ncssm.iwp.math.{MCalculator, MCalculator_Parametric, MDataHistory, MVariables}
-import edu.ncssm.iwp.objects.DObject_Solid
+import edu.ncssm.iwp.objects.{DObject_Solid, DObject_Time}
 import edu.ncssm.iwp.problemdb.{DProblemManager, DProblemState}
 import play.api._
 import play.api.libs.json.{JsNumber, JsObject, JsString, Json}
 import play.api.mvc._
+
+import scala.collection.mutable
 
 
 @Singleton
@@ -36,9 +38,9 @@ class DProblemController @Inject()(cc: ControllerComponents) extends AbstractCon
     o.setCalculatorY( calc )
 
 
-    val state = new DProblemState(dp)
+    val ps = new DProblemState(dp)
 
-    val vars = state.vars()
+    val vars = ps.vars()
 
     vars.setAtCurrentTick("tbwashere", 1.0)
     vars.setAtCurrentTick("delta_t", 1.0)
@@ -60,7 +62,40 @@ class DProblemController @Inject()(cc: ControllerComponents) extends AbstractCon
 
     val p = dpm.loadFile(path)
 
-    Ok(s"View: ${path},  p : ${p}")
+    val ps = new DProblemState(p)
+
+    // 2018Mar07 - This logic needs to be ported to IWP5
+    p.reorderProblemObjectsBySymbolicDependency()
+
+    // zero all the objects in the problem.
+
+    ps.setTickInProgress(true)
+    // This sets the forward direction, previously connect to the play button in swing
+
+
+// This actually advances the clock
+//    ps.tickStepForward()
+
+
+    val time = p.getTimeObject
+
+    val frames = mutable.Queue[(Double, String)]()
+
+    while (time.getTime < time.getStopTime ) {
+
+      time.tick(ps)
+
+      val t = time.getTime
+
+      frames.enqueue((t, s"Time ${t}\n"))
+
+      Logger.info(s"DProblemConroller:88> Time: ${time.getTime}")
+      ps.tickStepForward()
+
+    }
+
+
+    Ok(s"View: ${path},  frames: ${frames},  p : ${p}")
 
   }
 
