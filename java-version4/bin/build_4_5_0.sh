cb24568e116adaa392c817b845e8265a11a1dec7
Taylor Brockman <taylor.brockman@gmail.com>
2018-03-15 14:50:11 -0400

    Converted animation to 4.5.0

--- java-version4/bin/build_4_5_0.sh
+++ java-version4/bin/build_4_5_0.sh
@@ -2,5 +2,8 @@ ant jar
 
 JAR=./dist/iwp-4_5_0.jar
 # java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/equilibrium-03.iwp 
-java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/040305.iwp
+#java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/040305.iwp
+
+#java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/pulley-plane-03.iwp
+java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/equilibrium-03b.iwp
 
