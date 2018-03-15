df273c400674f11f74246731d570f14640e6245d
Taylor Brockman <taylor.brockman@gmail.com>
2018-03-15 00:49:21 -0400

    Useful script

---
+++ java-version4/bin/build_4_5_0.sh
@@ -0,0 +1,6 @@
+ant jar 
+
+JAR=./dist/iwp-4_5_0.jar
+# java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/equilibrium-03.iwp 
+java -cp $JAR edu.ncssm.iwp.bin.IWP5_Rewriter ../animations/winters-ncssm-2009/040305.iwp
+
