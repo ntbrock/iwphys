560052e349e70f405c50e3af5158c61302699bc6
Taylor Brockman <taylor.brockman@gmail.com>
2018-11-30 11:03:16 -0500

    Iwp6 Deployment

--- play-version6/schema/samples/postIwp6.sh
+++ play-version6/schema/samples/postIwp6.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl -X POST \
-	-d @coulombslaw03.iwp.json \
+	-d @apparent-depth-6.iwp.json \
 	-H "Content-type: application/json" \
-	http://iwp6.iwphys.org:8470/animations/winters-ncssm-2009/coulombslaw03.iwp
+	http://iwp6.iwphys.org:8470/animations/winters-ncssm-2009/apparent-depth-6.iwp
 
