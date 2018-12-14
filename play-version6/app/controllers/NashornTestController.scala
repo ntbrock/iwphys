5ee364b4136d1ca056165f044f15d97c76acdd03
Taylor Brockman <taylor.brockman@gmail.com>
2018-12-14 12:10:20 -0500

    More informative unit test routes

--- play-version6/app/controllers/NashornTestController.scala
+++ play-version6/app/controllers/NashornTestController.scala
@@ -23,7 +23,7 @@ class NashornTestController @Inject()(cc: ControllerComponents, mongo: IwpMongoC
 
 
 
-  def nashornTest1() = Action { implicit request: Request[AnyContent] =>
+  def nashornTest1_eval() = Action { implicit request: Request[AnyContent] =>
 
 
     val engine = new ScriptEngineManager().getEngineByName("nashorn")
@@ -39,7 +39,7 @@ class NashornTestController @Inject()(cc: ControllerComponents, mongo: IwpMongoC
 
   // Read + Execute from a file.
 
-  def nashornTest2() = Action { implicit request: Request[AnyContent] =>
+  def nashornTest2_file() = Action { implicit request: Request[AnyContent] =>
 
     val engine = new ScriptEngineManager().getEngineByName("nashorn")
 
@@ -63,7 +63,7 @@ class NashornTestController @Inject()(cc: ControllerComponents, mongo: IwpMongoC
 
   // Math.js
 
-  def nashornTest3() = Action { implicit request: Request[AnyContent] =>
+  def nashornTest3_loadMathJs() = Action { implicit request: Request[AnyContent] =>
 
     val engine = new ScriptEngineManager().getEngineByName("nashorn")
 
@@ -90,7 +90,7 @@ class NashornTestController @Inject()(cc: ControllerComponents, mongo: IwpMongoC
 
   // Execute the iwp5, and jquery animator javascript
 
-  def nashornTest4() = Action { implicit request: Request[AnyContent] =>
+  def nashornTest4_loadIwpAnimation() = Action { implicit request: Request[AnyContent] =>
 
     val engine = new ScriptEngineManager().getEngineByName("nashorn")
 
@@ -121,6 +121,43 @@ class NashornTestController @Inject()(cc: ControllerComponents, mongo: IwpMongoC
 
   }
 
+
+
+
+
+
+  // Bring some calculations out!
+
+  def nashornTest5_calcIwpSteps() = Action { implicit request: Request[AnyContent] =>
+
+    val engine = new ScriptEngineManager().getEngineByName("nashorn")
+
+    // Jquery 3 does not work in Nashorn: Cannot read property "createElement" from undefined in <eval> at line number 2]
+    // val jqFilereader = new FileReader("public/javascripts/jquery-3.2.1.min.js")
+    // val jqEval = engine.eval(jqFilereader)
+
+    val mathJsReader = new FileReader("public/javascripts/math.min.js")
+    val mathJsEval = engine.eval(mathJsReader)
+
+    val iwpCalcReader = new FileReader("public/javascripts/iwp/iwp6-calc.js")
+    val iwpCalcEval = engine.eval(iwpCalcReader)
+
+    val iwpReadReader = new FileReader("public/javascripts/iwp/iwp6-read.js")
+    val iwpReadEval = engine.eval(iwpReadReader)
+
+
+    val invokable = engine.asInstanceOf[Invocable]
+
+
+    // https://www.iwphys.org/xtoj.php/unit-test-2017/TEST_euler.iwp
+    val unitTestAnimation = "{\"author\":{\"username\":{},\"name\":{},\"organization\":{},\"email\":{}},\"objects\":{\"time\":{\"start\":\"0.0\",\"stop\":\"100.0\",\"change\":\"0.02\",\"fps\":\"15.0\"},\"GraphWindow\":{\"xmin\":\"-10.0\",\"xmax\":\"10.0\",\"ymin\":\"-10.0\",\"ymax\":\"10.0\",\"xgrid\":\"1.0\",\"ygrid\":\"1.0\"},\"window\":{\"xmin\":\"-10.0\",\"xmax\":\"10.0\",\"ymin\":\"-10.0\",\"ymax\":\"10.0\",\"xgrid\":\"1.0\",\"ygrid\":\"1.0\",\"xunit\":\"m\",\"yunit\":\"m\",\"showAllDataAvailable\":\"false\",\"drawGridNumbers\":\"true\"},\"description\":{\"text\":\"Very simple test for Euler calculations. It's important to remember that the Initial Displacement and Velocity can be dynamic equations and \\npossibly more complicated than just fixed numbers. This means they would be based on inputs values, which is illustrated here.\"},\"input\":[{\"name\":\"initxdisp\",\"text\":\"Initial X Displacement\",\"initialValue\":\"-10.0\",\"units\":\"m\"},{\"name\":\"initxvel\",\"text\":\"Initial X Velocity\",\"initialValue\":\"5.0\",\"units\":\"m\"}],\"solid\":{\"name\":\"EulerMover\",\"shape\":{\"@attributes\":{\"type\":\"circle\",\"drawTrails\":\"false\",\"drawVectors\":\"false\"},\"vectors\":{\"@attributes\":{\"yAccel\":\"false\",\"Vel\":\"false\",\"xAccel\":\"false\",\"yVel\":\"false\",\"xVel\":\"false\",\"Accel\":\"false\"}},\"width\":{\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"1\"}},\"height\":{\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"1\"}},\"angle\":{\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"0\"}},\"graphOptions\":{\"@attributes\":{\"graphVisible\":\"false\"},\"initiallyOn\":{\"@attributes\":{\"yAccel\":\"false\",\"yVel\":\"false\",\"xAccel\":\"false\",\"xVel\":\"false\",\"yPos\":\"false\",\"xPos\":\"false\"}}}},\"color\":{\"red\":\"255\",\"green\":\"0\",\"blue\":\"0\"},\"xpath\":{\"calculator\":{\"@attributes\":{\"type\":\"euler\"},\"displacement\":\"initxdisp\",\"velocity\":\"initxvel\",\"acceleration\":\"-5*t\"}},\"ypath\":{\"calculator\":{\"@attributes\":{\"type\":\"euler\"},\"displacement\":\"-10\",\"velocity\":\"0\",\"acceleration\":\"5\"}}},\"output\":[{\"name\":\"EulerXDispOutput\",\"text\":\"X Disp\",\"units\":\"m\",\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"EulerMover.xdisp\"}},{\"name\":\"EulerXVelOutput\",\"text\":\"X Vel\",\"units\":\"m\",\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"EulerMover.xvel\"}},{\"name\":\"EulerXAccelOutput\",\"text\":\"X Accel\",\"units\":\"m\",\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"EulerMover.xaccel\"}},{\"name\":\"EulerYVelOutput\",\"text\":\"Y Vel\",\"units\":\"m\",\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"EulerMover.yvel\"}},{\"name\":\"EulerYDispOutput\",\"text\":\"Y Disp\",\"units\":\"m\",\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"EulerMover.ydisp\"}},{\"name\":\"EulerYAccelOutput\",\"text\":\"Y Accel\",\"units\":\"m\",\"calculator\":{\"@attributes\":{\"type\":\"parametric\"},\"value\":\"EulerMover.yaccel\"}}]}}"
+
+    Logger.info(s"NashornTestController:51> invokable: ${invokable}")
+    val r = invokable.invokeFunction("readAnimationString", unitTestAnimation )
+
+    Ok(s"Nashorn Test 4: calcEval: ${iwpCalcEval},  readEval: ${iwpReadEval}  function call to readAnimationObject: ${r} ")
+
+  }
 
 
 }
