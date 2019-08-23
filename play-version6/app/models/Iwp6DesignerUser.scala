015e52c6ae0c26da3a3ba51a4d21d3df799636e9
Taylor Brockman <taylor.brockman@gmail.com>
2019-08-23 17:41:51 -0400

    Fix 2

--- play-version6/app/models/Iwp6DesignerUser.scala
+++ play-version6/app/models/Iwp6DesignerUser.scala
@@ -1,5 +1,15 @@
 package models
 
-class Iwp6DesignerUser {
+import java.time.ZonedDateTime
 
-}
+case class Iwp6DesignerUser ( email: String,
+                              displayName: String,
+                              locationName: Option[String],
+                              schoolName: Option[String],
+                              personalBiography: Option[String],
+                              userAvatarUrl: Option[String],
+                              createdOn: ZonedDateTime,
+                              signInToken: Option[String] = None,
+                              lastSignedInOn: Option[ZonedDateTime] = None )
+
+
