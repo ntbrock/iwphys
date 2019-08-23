c53a5401d0c255437126b190d35807eba9565e4e
Taylor Brockman <taylor.brockman@gmail.com>
2019-08-23 17:40:48 -0400

    Fix the build

--- play-version6/app/controllers/DesignerTestController.scala
+++ play-version6/app/controllers/DesignerTestController.scala
@@ -1,9 +1,12 @@
 package controllers
 
+import java.time.ZonedDateTime
+
 import edu.ncssm.iwp.plugin.IWPObject
 import edu.ncssm.iwp.problemdb.DProblemXMLParser
 import javax.inject._
-import models.Iwp6Animation
+import models.{Iwp6Animation, Iwp6Author, Iwp6DesignerUser}
+import play.api.libs.json.Json
 import play.api.mvc._
 import services.IwpFilesystemBrowserService
 
@@ -15,136 +18,19 @@ class DesignerTestController @Inject()(cc: ControllerComponents,
                                        iwpFilesystemBrowserService: IwpFilesystemBrowserService
                                  ) extends AbstractController(cc) {
 
+  implicit val jsfIwp6DesignerUserFormat = Json.format[Iwp6DesignerUser]
 
-  // DProblem problem = DProblemXMLParser.load ( data.toString ( ) );
+  def userSample() = Action { implicit request =>
 
-  /**
-    * Note this:
-    * //---------------------------------------------------------------
-    * //---------------------------------------------------------------
-    * /**
-    * IWP 3 - on initial zero of the problem, re-order the obejcts in the
-    * problem based on their variable dependency.
-    * 2007-Jan-29 brockman
-    */
-    * *
-    * public synchronized Unit reorderProblemObjectsBySymbolicDependency ()
-    * throws UnknownVariableException, CircularDependencyException, InvalidEquationException
-    * {
-    * @return
-    */
+    val taylor = Iwp6DesignerUser( email = "taylor.brockman@gmail.com",
+      displayName = "Taylor Brockman",
+      locationName = Some("Charleston, SC"),
+      schoolName = Some("NCSSM"),
+      personalBiography = Some("Class of 1999! Back in the day"),
+      userAvatarUrl = None,
+      createdOn = ZonedDateTime.now )
 
-  def version4ReadString() = Action { implicit request: Request[AnyContent] =>
-
-    // Read Xml
-    val simpleXml = "<problem><author><username>username</username><name>name</name><organization>org</organization><email></email></author><objects><time>t1</time><window>w1</window><output>o1</output><output>o2</output><solid>s1</solid><output>o3</output></objects></problem>"
-
-    val dproblem = DProblemXMLParser.load(simpleXml)
-
-    dproblem.objectsInDrawOrder
-
-    Ok(s"version4ReadString to DProblem: ${dproblem}")
-
-  }
-
-
-
-
-  def version4CollectionRead(collection: String, filename: String) = Action { request =>
-
-
-    iwpFilesystemBrowserService.getCollection(collection) match {
-      case None => NotFound(s"No collection: ${collection}")
-      case Some(collection) =>
-
-        iwpFilesystemBrowserService.getVersion4Problem(collection, filename) match {
-
-          case Failure(x) => BadRequest(s"Failure loading problem: ${x}")
-          case Success(dproblem) =>
-
-
-            val drawOrder = dproblem.getObjectsForDrawing.toArray.toSeq
-              .map{ ar => ar.asInstanceOf[IWPObject] }
-              .map{ o => s"name: ${o.getName} class: ${o.getClass.getName}" }
-              .mkString("\n")
-
-            val tickOrder = dproblem.getObjectsForTicking.toArray.toSeq.mkString("\n")
-
-
-            Ok(s"version4DirectoryRead to DProblem:\n\n${dproblem}\n\nDrawOrder: ${drawOrder}\n\nTickOrder: ${tickOrder}")
-
-
-        }
-
-
-    }
-
-
-  }
-
-
-  def version4SubCollectionRead(collection: String, subCollection: String, filename: String) = Action { request =>
-
-
-    iwpFilesystemBrowserService.getCollection(s"${collection}/${subCollection}") match {
-      case None => NotFound(s"No collection: ${collection}")
-      case Some(collection) =>
-
-        iwpFilesystemBrowserService.getVersion4Problem(collection, filename) match {
-
-          case Failure(x) => BadRequest(s"Failure loading problem: ${x}")
-          case Success(dproblem) =>
-
-
-            val drawOrder = dproblem.getObjectsForDrawing.toArray.toSeq
-              .map{ ar => ar.asInstanceOf[IWPObject] }
-              .map{ o => s"name: ${o.getName} class: ${o.getClass.getName}" }
-              .mkString("\n")
-
-            val tickOrder = dproblem.getObjectsForTicking.toArray.toSeq.mkString("\n")
-
-
-            Ok(s"version4DirectoryRead to DProblem:\n\n${dproblem}\n\nDrawOrder: ${drawOrder}\n\nTickOrder: ${tickOrder}")
-
-        }
-
-    }
-
-  }
-
-
-
-  def version4SubCollectionConvert(collection: String, subCollection: String, filename: String) = Action { request =>
-
-    iwpFilesystemBrowserService.getCollection(s"${collection}/${subCollection}") match {
-      case None => NotFound(s"No collection: ${collection}")
-      case Some(collection) =>
-
-        iwpFilesystemBrowserService.getVersion4Problem(collection, filename) match {
-
-          case Failure(x) => BadRequest(s"Failure loading problem: ${x}")
-          case Success(dproblem) =>
-
-            // Transform to a Iwp6Animation
-
-            Iwp6Animation.fromDProblem(dproblem) match {
-              case Failure(x) => BadRequest(s"Unable to Convert DProblem to Animation: ${x}")
-
-              case Success(animation) => {
-                val json = Iwp6Animation.toJson(animation)
-
-                Ok(json).as("application/json")
-
-              }
-            }
-
-            //Ok(s"version4SubCollectionConvert to Iwp6Animation: ${animation}");
-
-        }
-
-
-    }
-
+    Ok(Json.toJson(taylor))
 
   }
 
