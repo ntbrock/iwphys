f5ca7368993b06addfa8771156fa3c76420a8880
Taylor Brockman <taylor.brockman@gmail.com>
2020-03-20 16:17:34 -0400

    Evaluation String math.js concept

--- animatorjs-version7/src/math.js
+++ animatorjs-version7/src/math.js
@@ -1,14 +1,8 @@
 const { evaluate } = require('mathjs');
 
-/*
-import {
-  atan2, chain, derivative, e, evaluate, log, pi, pow, round, sqrt
-} from 'mathjs'
-*/
-
-
 function math_sum(a,b) {
-	return evaluate(a+'+'+b);
+	var scope = { a : a, b : b };
+	return evaluate('a+b', scope);
 }
 
 module.exports = math_sum;
