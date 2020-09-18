b9c8eae71c36e8a839282b2dd9b38af160ff713d
Taylor Brockman <taylor.brockman@brainpowersoftware.com>
2020-09-18 16:42:33 -0400

    Refactor - Relocated Svg functions to a new animation-svg module

--- animatorjs-version7/test/iwp7/test-animation-calc.js
+++ animatorjs-version7/test/iwp7/test-animation-calc.js
@@ -6,24 +6,9 @@ let animationCalc = require('../../src/iwp7/animation-calc')
 
 describe('Animation', function () {
     describe('Calculator', function () {
-        it('xWidth translate from a coordinate system into canvas position', function () {
+        it('Jonathan Needs to find a function to test!', function () {
 
-            animationCalc.setAnimationWindow( {
-                "xmin": -10,
-                "xmax": 10,
-                "ymin": -10,
-                "ymax": 10,
-                "xgrid": 2,
-                "ygrid": 2,
-                "xunit": "meters",
-                "yunit": "meters",
-                "objectType": "window"
-            })
-
-            assert.strictEqual( animationCalc.xWidth(0) , 0 );
-            assert.strictEqual( animationCalc.xWidth(0.5) , 25 );
-            assert.strictEqual( animationCalc.xWidth(1) , 50 );
-            assert.strictEqual( animationCalc.xWidth(-1) , -50 );
+            assert.strictEqual( true, false );
 
         });
     });
