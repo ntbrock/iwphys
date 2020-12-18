61fca3cd9472548bad5337ea5741a38b65a5ca9a
Taylor Brockman <taylor.brockman@brainpowersoftware.com>
2020-12-18 13:43:11 -0500

    Browserify - A sample animation loads into teh browser, we're now attempting to compile

---
+++ animatorjs-version7/README.md
@@ -0,0 +1,7 @@
+
+## 2020Dec18
+
+Next : Together Get Animation Parsing stood up, then be able to begin making calls to Illustrator in browser.js!
+
+Continue here on 12/22 @ 2:45PM!
+
--- animatorjs-version7/src/browser.js
+++ animatorjs-version7/src/browser.js
@@ -1,16 +1,28 @@
 /**
- * Entry Point for webbrowser loads
+ * Interactive Web Physics Version 7
+ * 2020Dec18 Burgess , Brockman
+ * Entry Point for webbrowser loads, bundled via browserify, dev env : bin/beefyListen.sh
+ *
  */
 
+// D-Fense, require window global object
 if ( window === undefined ) {
-	throw Error("iwp browser.js designed to eb invoked from a browser with a 'window' global")
+	throw Error("iwp browser.js designed to be invoked from a browser with a 'window' global")
 }
 
 const $ = require('jquery')
 
-console.log("browser.js:6> Hello World");
-
 const animationIllustrate = require('./iwp7/animation-illustrate')
+const animationPlayer = require('./iwp7/animation-player')
+const animationCompile = require('./iwp7/animation-compile')
+
+
+// Metacode: read simple Animation Json  (Todo , dynamic from page / rest endpoint )
+
+const testJson = require('./animations/simple-two-boxes.json')
+const animation = animationCompile.compileAnimationFromJson( testJson );
+
+console.log("Test Animation: " , animation);
 
 
 
